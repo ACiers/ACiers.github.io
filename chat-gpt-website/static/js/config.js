ed4e8e21b83f1580fcf9cbe516688f09050bcc93
ACiers <132809075+ACiers@users.noreply.github.com>
2023-06-11 15:54:18 +0800

    更新完成

--- chat-gpt-website/static/js/config.js
+++ chat-gpt-website/static/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
     url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: "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"  // openAi Base64 编码 apiKey
+    apiKey: "sk-hsPDEbjAzjJZfRSa2b851aBc765a4fB3BeCb56418736D6F6"  // openAi Base64 编码 apiKey
 };  
